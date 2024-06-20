874335f5108cc40d1cbde24c4f44a0e5698331c7
DecimTheJudge <paolo1piga@gmail.com>
2024-06-20 16:53:54 +0200

    aggiunto l'event listener

--- ex/main.js
+++ ex/main.js
@@ -58,7 +58,15 @@ const posts = [
 
 const container = document.getElementById(`container`);
 
+let likeCounter = document.getElementById(`like-counter-1`);
 
+// aggiungo l'event listener per i like
+likeCounter.addEventListener("click", function(){
+    posted.likes++;
+   
+    moreLike = posted.likes + likeCounter;
+    likeCounter.classList.add(".like-button--liked");
+})
 
 // creo un foreach per l'array
 posts.forEach((posted) => {
@@ -91,7 +99,7 @@ posts.forEach((posted) => {
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${posted.likes}</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${moreLike}</b> persone
                     </div>
                 </div> 
             </div>            
@@ -101,33 +109,3 @@ posts.forEach((posted) => {
 } )
 
 
-{/* <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">Phil Mangione</div>
-                        <div class="post-meta__time">4 mesi fa</div>
-                    </div>                    
-                </div>
-            </div>
-            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-            <div class="post__image">
-                <img src="https://unsplash.it/600/300?image=171" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div> */}
